850efd0177bba9aac3f4f817f765e687d03e3353
lassouani <lassouani_sofiane@outlook.com>
2016-11-01 13:09:19 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 package AAproject.src;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -7,24 +6,13 @@ import java.util.Iterator;
 
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	ArrayList<Card> sol = new ArrayList<Card>();
+		ArrayList<Card> sol = new ArrayList<Card>();
 		Cards h = new Cards();
-		h.generateHand(10, 3);
-		System.out.println(h.toString());
-		
-		sol=h.Recherche(h.hand, h.FirstElement());	
-		
-		System.out.println("la solution est premiere carte"+sol.toString());
-		
-		
-		
-		
-		
-		
-
+		h.generateHand(10, 3);	
+		h.play();
 	}
 
 }
