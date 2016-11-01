01566b60b6740da0412c62818a1a719bc38b9173
lassouani <lassouani_sofiane@outlook.com>
2016-11-01 13:08:51 +0100

    Update Cards.java

--- src/Cards.java
+++ src/Cards.java
@@ -1,98 +1,73 @@
 package AAproject.src;
+
 import java.awt.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
-
 public class Cards {
 
-	//private Set<Card> hand = new HashSet<Card>();
-	  public ArrayList<Card> hand = new ArrayList<Card>();
-	  
-	  public ArrayList<Card> sequence = new ArrayList<Card>();
-		boolean find=true;
-	
-	public Cards() {
-		
-	}
-	
-	public ArrayList<Card> generateHand(int number_of_cards, int number_of_colors) {
-		
-		Card c =new Card(number_of_cards, number_of_colors);
-		
-		for(int i = 0 ; i< number_of_cards; i++) {
+	// private Set<Card> hand = new HashSet<Card>();
+	public ArrayList<Card> hand = new ArrayList<Card>();
+	public ArrayList<Card> sequence = new ArrayList<Card>();
+	boolean find = true;
+	int counter = 0;
+
+	public Cards() {}
+
+	/*
+	 * generate a set of cards
+	 */
+	public ArrayList<Card> generateHand(int number_of_cards,
+			int number_of_colors) {
+
+		Card c = new Card(number_of_cards, number_of_colors);
+		for (int i = 0; i < number_of_cards; i++) {
 			hand.add(c.generateCard(number_of_colors));
 		}
-		
 		return hand;
 	}
 	
-	public String toString() {
-		String result="";
-		Iterator i=hand.iterator(); 
-		while(i.hasNext()) 
-		{
-			result+= i.next().toString(); 
+
+	// Recursive method ( Stops when hand is empty )
+	public void play() {
+		Card c = getHand(0);
+		counter++;
+		System.out.println("----------- STEP:" + counter + "-----------\n");
+		System.out.println("The hand is :");
+		printHand();
+		System.out.println("The hand to play is :");
+		ArrayList<Card> cardstoplay = new ArrayList<Card>();
+		for (Card card : this.hand) {
+			if (card.getCard_number() == c.getCard_number()
+					|| card.getCard_color() == c.getCard_color())
+				cardstoplay.add(card);
 		}
-		return result;
+		for (Card cc : cardstoplay) {
+			cc.print();
+			hand.remove(cc);
+		}
+		try {
+			Thread.currentThread().sleep(5000);
+		} catch (InterruptedException e) {
+		}
+		if (hand.size() > 0)
+			play();
+		else
+			System.out.println("------Done-----");
+
 	}
-	
-	
-	public Card FirstElement(){
-		return hand.get(0);
+
+	public void printHand() {
+		for(Card card : hand)
+			System.out.println(card.getCard_number()+"  "+card.getCard_color());
 	}
-	
-	
-	public ArrayList<Card> Recherche(ArrayList<Card> hand, Card racine) {
-		  if (!hand.isEmpty()) {
-			
-					Iterator i=hand.iterator(); 
-					while(i.hasNext() & find==true) {
-						
-						//enlever la racine de l'ensemble et le metre dans sequnce
-						//faire en sorte qu'il enleve qu'une instance
-						if (i.equals(racine)) {
-							Card x=(Card) i.next();
-							sequence.add(x);
-							i.remove();
-							find=false;
-						}
-							}
-					Iterator j=hand.iterator(); 
-					while(j.hasNext()) 
-					{
-						Card x=(Card) j.next();
-						
-						if ((x.getCard_number()==racine.getCard_number() )|| (x.getCard_color()==racine.getCard_color())) {
-							Recherche(hand, x);
-							
-						}
-					}
-				
-				}else {
-					
-				}
-				return sequence;
-					
-					
-				}
-	
-	
-	
-	
-	
+
+	public Card getHand(int i) {
+		// TODO Auto-generated method stub
+		if (hand.size() == 0)
+			return null;
+		return hand.get(i);
+	}
+
 }
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-
-
